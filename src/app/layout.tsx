447e2a0fa981030b88fe943e44f7b522e36fe8e6
ahmedsamyalfifi123 <ahmedsamyalafifi123@gmail.com>
2025-12-29 00:33:40 +0200

    daark mode for browsers in phone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,10 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+    { media: "(prefers-color-scheme: dark)", color: "#09090b" },
+  ],
 };
 
 export default function RootLayout({
